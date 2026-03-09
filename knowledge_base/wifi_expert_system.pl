dc179cb984dba72fdddf46292d880b5611eb94ea
jeffwhite099 <jephthahpeprah099@gmai.coml>
2026-03-09 14:34:47 +0000

    feat: Facts

--- knowledge_base/wifi_expert_system.pl
+++ knowledge_base/wifi_expert_system.pl
@@ -13,3 +13,7 @@ question(connected_to_wifi, 'Does it show connected to Wi-Fi?').
 question(internet_access, 'Does the internet work on the laptop?').
 question(other_devices_connect, 'Do other devices connect to that same Wi-Fi?').
 question(other_devices_internet, 'Do other devices have internet on that Wi-Fi?').
+
+% ask/2 predicate
+ask(Fact, Answer) :-
+    known(Fact, Answer), !.
